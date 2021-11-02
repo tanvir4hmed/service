6fbbc43975e93d8bdd2966ddb39cb744b099611b
tanvir4hmed <tanvir4hmed@gmail.com>
2021-11-02 15:11:25 +0600

    Update app.py
    Return message change for app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return 'Hey, we have Flask in a Docker container!'
+        return 'Welcome to the Docker container of Service'
 
 
 if __name__ == '__main__':
